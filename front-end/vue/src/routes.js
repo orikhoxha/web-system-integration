103407339c907a46341c61988f97bc7ca66359be
Orik Hoxha <ohoxha@hawk.iit.edu>
2018-11-13 00:01:13 -0500

    Component Added: Favourite Items

--- front-end/vue/src/routes.js
+++ front-end/vue/src/routes.js
@@ -1,5 +1,5 @@
 import Home from './components/Home.vue';
-import Favourites from './components/favourites/Favourites.vue';
+import FavouriteItems from './components/items/FavouriteItems.vue';
 import Departments from './components/department/Departments.vue';
 import Items from './components/items/Items.vue';
 import ItemDetails from './components/items/ItemDetails.vue';
@@ -9,6 +9,6 @@ export const routes = [
             { path: '/', component: Items},
             { path: '/item/:id', component: ItemDetails}
         ]},
-    { path: '/favourites', component: Favourites},
-    { path: '/departments', component: Departments}
+    { path: '/favourites', component: FavouriteItems},
+    { path: '/departments', component: Departments},
 ]
